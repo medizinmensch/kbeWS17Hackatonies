da9f78888ee324f383e907456840c5f412a580b2
s0553414 <s0553414@saturn7-linux.wh.f4.htw-berlin.de>
2017-10-26 13:51:35 +0200

    added RunMeRunner project

---
+++ runMeRunner/src/test/java/htw/ii/kbe/hackatonies/runMeRunner/ReadConfigFileTest.java
@@ -0,0 +1,15 @@
+package htw.ii.kbe.hackatonies.runMeRunner;
+
+import org.junit.*;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ReadConfigFileTest {
+	
+	@Test
+	public void ValidateReturn() {
+		assertTrue()
+	}
+	
+}
